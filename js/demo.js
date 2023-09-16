112fd799c5b037a027c236e38b3f25e5b28f82f4
mohamedsaeed22 <mohamed.saeed1006@gmail.com>
2023-09-16 07:16:22 +0300

    updated home again

--- js/demo.js
+++ js/demo.js
@@ -57,3 +57,98 @@ for (let index = 1; index <= 12; index++) {
 </div>
   `;
 }
+
+// ====================== start products ========================
+
+function getAllProducts() {
+  fetch("../js/products.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((products) => {
+      renderProducts(products);
+    })
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+}
+getAllProducts();
+
+function getProductsByCategory(category) {
+  fetch("../js/products.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((products) => {
+      var filteredArr = products.filter((product) => {
+        return product.category === category;
+      });
+      renderProducts(filteredArr);
+    })
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+}
+
+var cardsContainer = document.getElementById("cards-container");
+function renderProducts(products) {
+  cardsContainer.innerHTML = "";
+  products.forEach((product) => {
+    cardsContainer.innerHTML += `
+    <div class="card">
+    <div class="img-wrapper">
+      <img src="${product.image}" alt="product-image" loading="lazy" />
+      <div class="content">
+        <p class="title">
+         ${product.title}
+        </p>
+        <p class="price">
+          <span>EGP</span>
+          <span>${product.price}</span>
+        </p>
+        <span class="addToCart"
+          ><i class="fa-solid fa-cart-plus"></i
+        ></span>
+      </div>
+    </div>
+  </div>
+    `;
+  });
+}
+
+var filters = document.querySelectorAll(".filter span");
+filters.forEach((el) => {
+  el.addEventListener("click", () => {
+    filters.forEach((span) => {
+      span.classList.remove("active");
+    });
+    el.classList.add("active");
+    filterCards(el);
+  });
+});
+
+function filterCards(el) {
+  if (el.id == "phone") {
+    getProductsByCategory("phone");
+  } else if (el.id == "sneaker") {
+    getProductsByCategory("sneaker");
+  } else if (el.id == "laptop") {
+    getProductsByCategory("laptop");
+  } else if (el.id == "makeup") {
+    getProductsByCategory("makeup");
+  } else if (el.id == "all") {
+    getAllProducts();
+  }
+}
+
+// ================== cart click =================
+var cart = document.getElementById("cart");
+var cart_container = document.getElementById("cart-container");
+cart.addEventListener("click", () => {
+  cart_container.style.display = "block";
+});
+
+var closeCart = document.getElementById("close-cart");
+closeCart.addEventListener("click", () => {
+  cart_container.style.display = "none";
+});
